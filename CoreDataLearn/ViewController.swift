c532d19300ac7c168b72e7764f410ed751827d15
vagrawal1986 <jadu.vivek@gmail.com>
2019-02-27 07:00:42 -0600

    Initial Commit

---
+++ CoreDataLearn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataLearn
+//
+//  Created by Vivek Agrawal on 2/27/19.
+//  Copyright © 2019 Vivek Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
